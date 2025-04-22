d827ea0f1d470959fa38818e298e68f4ef92aa49
Andrew Vo <voandrew.khoa@gmail.com>
2025-04-21 17:47:10 -0700

    Add arrow.nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -71,64 +71,63 @@ require('lazy').setup({
     "jake-stewart/multicursor.nvim",
     branch = "1.0",
     config = function()
-        local mc = require("multicursor-nvim")
-        mc.setup()
+      local mc = require("multicursor-nvim")
+      mc.setup()
 
-        local set = vim.keymap.set
+      local set = vim.keymap.set
 
-        -- Add or skip cursor above/below the main cursor.
-        set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end, { desc = "Add cursor up" })
-        set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end, { desc = "Add cursor down" })
-        set({"n", "x"}, "<leader><up>", function() mc.lineSkipCursor(-1) end, { desc = "Skip cursor up" })
-        set({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end, { desc = "Skip cursor down" })
+      -- Add or skip cursor above/below the main cursor.
+      set({ "n", "x" }, "<up>", function() mc.lineAddCursor(-1) end, { desc = "Add cursor up" })
+      set({ "n", "x" }, "<down>", function() mc.lineAddCursor(1) end, { desc = "Add cursor down" })
+      set({ "n", "x" }, "<leader><up>", function() mc.lineSkipCursor(-1) end, { desc = "Skip cursor up" })
+      set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end, { desc = "Skip cursor down" })
 
-        -- Add or skip adding a new cursor by matching word/selection
-        -- set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end, { desc = "Add match cursor down" })
-        -- set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end, { desc = "Add match cursor up" })
-        -- set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end, { desc = "Skip match cursor down" })
-        -- set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end, { desc = "Skip match cursor up" })
+      -- Add or skip adding a new cursor by matching word/selection
+      -- set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end, { desc = "Add match cursor down" })
+      -- set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end, { desc = "Add match cursor up" })
+      -- set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end, { desc = "Skip match cursor down" })
+      -- set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end, { desc = "Skip match cursor up" })
 
-        -- Add and remove cursors with control + left click.
-        set("n", "<c-leftmouse>", mc.handleMouse)
-        set("n", "<c-leftdrag>", mc.handleMouseDrag)
-        set("n", "<c-leftrelease>", mc.handleMouseRelease)
+      -- Add and remove cursors with control + left click.
+      set("n", "<c-leftmouse>", mc.handleMouse)
+      set("n", "<c-leftdrag>", mc.handleMouseDrag)
+      set("n", "<c-leftrelease>", mc.handleMouseRelease)
 
-        -- Disable and enable cursors.
-        set({"n", "x"}, "<c-q>", mc.toggleCursor)
+      -- Disable and enable cursors.
+      set({ "n", "x" }, "<c-q>", mc.toggleCursor)
 
-        -- match new cursors within visual selections by regex.
-        set("x", "M", mc.matchCursors)
+      -- match new cursors within visual selections by regex.
+      set("x", "M", mc.matchCursors)
 
-        -- Mappings defined in a keymap layer only apply when there are
-        -- multiple cursors. This lets you have overlapping mappings.
-        mc.addKeymapLayer(function(layerSet)
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one.
+        layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+        layerSet({ "n", "x" }, "<right>", mc.nextCursor)
 
-            -- Select a different cursor as the main one.
-            layerSet({"n", "x"}, "<left>", mc.prevCursor)
-            layerSet({"n", "x"}, "<right>", mc.nextCursor)
+        -- Delete the main cursor.
+        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
 
-            -- Delete the main cursor.
-            layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
-
-            -- Enable and clear cursors using escape.
-            layerSet("n", "<esc>", function()
-                if not mc.cursorsEnabled() then
-                    mc.enableCursors()
-                else
-                    mc.clearCursors()
-                end
-            end)
+        -- Enable and clear cursors using escape.
+        layerSet("n", "<esc>", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
         end)
+      end)
 
-        -- Customize how cursors look.
-        local hl = vim.api.nvim_set_hl
-        hl(0, "MultiCursorCursor", { reverse = true })
-        hl(0, "MultiCursorVisual", { link = "Visual" })
-        hl(0, "MultiCursorSign", { link = "SignColumn"})
-        hl(0, "MultiCursorMatchPreview", { link = "Search" })
-        hl(0, "MultiCursorDisabledCursor", { reverse = true })
-        hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-        hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { reverse = true })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorMatchPreview", { link = "Search" })
+      hl(0, "MultiCursorDisabledCursor", { reverse = true })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
     end
   },
   -- NOTE: This is where your plugins related to LSP can be installed.
@@ -326,7 +325,19 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    "otavioschwanck/arrow.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      -- or if using `mini.icons`
+      -- { "echasnovski/mini.icons" },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = ';',      -- Recommended to be a single key
+      buffer_leader_key = 'm', -- Per Buffer Mappings
+    }
+  },
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
@@ -349,10 +360,10 @@ require('lazy').setup({
     'rmagatti/goto-preview',
     config = function()
       require('goto-preview').setup({
-      references = {
-        provider = 'snacks',
-      }
-    })
+        references = {
+          provider = 'snacks',
+        }
+      })
     end
   },
   {
