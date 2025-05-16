ee3ab4183e0c6c0d8fffae7f3161b9917743be53
Andrew Vo <voandrew.khoa@gmail.com>
2025-05-16 10:23:13 -0700

    Remove `m` key prefix requirement for surround

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -484,12 +484,12 @@ require('lazy').setup({
     config = function()
       require('mini.surround').setup {
         mappings = {
-          add = '<leader>msa',       -- Add surrounding in Normal and Visual modes
-          delete = '<leader>msd',    -- Delete surrounding
-          find = '<leader>msf',      -- Find surrounding (to the right)
-          find_left = '<leader>msF', -- Find surrounding (to the left)
-          highlight = '<leader>msh', -- Highlight surrounding
-          replace = '<leader>msr',   -- Replace surrounding
+          add = '<leader>sa',       -- Add surrounding in Normal and Visual modes
+          delete = '<leader>sd',    -- Delete surrounding
+          find = '<leader>sf',      -- Find surrounding (to the right)
+          find_left = '<leader>sF', -- Find surrounding (to the left)
+          highlight = '<leader>sh', -- Highlight surrounding
+          replace = '<leader>sr',   -- Replace surrounding
           update_n_lines = '',       -- Update `n_lines`
 
           suffix_last = '',          -- Suffix to search with "prev" method
